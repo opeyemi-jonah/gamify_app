ca1b1a86cdf8cd25f4ea74aec3da1bbcf87917b6
opeyemi-jonah <gavrieljonah@gmail.com>
2024-12-04 22:26:54 -0800

    initial commit; Top header home page

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:gamify_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gamify',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:gamify_app/data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _featureGamesWidget(),
+          _gradientBoxWidget(),
+          _topLayerWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _featureGamesWidget() {
+    return SizedBox(
+        height: _deviceHeight * 0.50,
+        width: _deviceWidth,
+        child: PageView(
+          scrollDirection: Axis.horizontal,
+          children: featuredGames
+              .map((game_) => Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(game_.coverImage.url),
+                    )),
+                  ))
+              .toList(),
+        ));
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.80,
+        width: _deviceWidth,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops: [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _topLayerWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.05, vertical: _deviceHeight * 0.005),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _topBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.13,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 30,
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+              SizedBox(
+                width: _deviceWidth * 0.03,
+              ),
+              const Icon(
+                Icons.notifications_none,
+                color: Colors.white,
+                size: 30,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
